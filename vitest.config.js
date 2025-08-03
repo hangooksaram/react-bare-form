147ea1462500e1f04b27988484adb255dcff1648
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-08-03 14:07:41 +0900

    core:add vite-tsconfig-paths plugin to config

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
   },
+  plugins: [tsconfigPaths()],
 });
