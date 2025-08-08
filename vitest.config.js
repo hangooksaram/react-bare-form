ca6ee43011a51a8ee229fa3a0d807b18e06d8d15
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-08-08 21:37:12 +0900

    chore:add test file pattern to Vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
   plugins: [tsconfigPaths()],
 });
