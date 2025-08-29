d99a9d9cdbfe5ea5d22467812d95c46d12035d2b
오현재 <hyunjae1995@gmail.com>
2025-08-29 16:42:01 +0900

    test:refactor and expand useValidate hook tests
    - hook init 함수 추가

--- src/__tests__/validate/useValidate.test.tsx
+++ src/__tests__/validate/useValidate.test.tsx
@@ -6,22 +6,66 @@ import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/vitest";
 
 import Form from "../../components/Form.test";
+import useRefs from "@/hooks/form/useRefs";
+import { ValidateResult } from "@/constants";
 
-describe("if validateSchema is not defined, not execute validation", () => {
-  let hookResult: ReturnType<typeof useValidate>;
+const initValidateHook = (params: any, schema?: any) => {
+  const { result } = renderHook(() => useValidate(params, schema));
+  return result.current;
+};
+
+const initHooks = () => {
+  const validateOff = initValidateHook({ name: "James" });
+  const validateOn = initValidateHook(
+    { name: "James" },
+    { name: { required: { message: "Name is required" } } }
+  );
+  const wrongValidate = initValidateHook(
+    { name: "" },
+    { name: { required: { message: "Name is required" } } }
+  );
+  return { validateOff, validateOn, wrongValidate };
+};
+
+const mockScrollFunc = () => {
+  const { result: refResult } = renderHook(() => useRefs());
+  refResult.current.scrollToErrorElement = vi.fn();
+};
+
+const screen = render(<Form />);
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+
+describe("useValidate", () => {
+  let hooks: ReturnType<typeof initHooks>;
+
   beforeEach(() => {
     vi.clearAllMocks();
-    const { result } = renderHook(() => useValidate({ name: "hi" }));
-    hookResult = result.current;
+    hooks = initHooks();
+    mockScrollFunc();
   });
 
-  it("should not execute validation", async () => {
-    hookResult.validateAndUpdateError = vi.fn();
-    const mockValidateFunc = hookResult.validateAndUpdateError;
-    const screen = render(<Form />);
+  it("should not execute validation when validate schema is not set", async () => {
+    hooks.validateOff.validate = vi.fn();
+    const mockValidateFunc = hooks.validateOff.validate;
 
-    expect(hookResult.isValidationOn).toBe(false);
-    await userEvent.type(screen.getByPlaceholderText("Enter name"), "hi");
+    expect(hooks.validateOff.isValidationOn).toBe(false);
+    await userEvent.type(screen.getByPlaceholderText("Enter name"), "James");
     expect(mockValidateFunc).not.toBeCalled();
+  });
+
+  it("should return invalid result when name is invalid", async () => {
+    await userEvent.click(screen.getByText("Submit"));
+
+    expect(hooks.wrongValidate.validate("name", "")).toBe(
+      ValidateResult.Invalid
+    );
+    expect(hooks.validateOn.validateAll()).toBe(ValidateResult.Valid);
+  });
+
+  it("should return valid result when name is valid", async () => {
+    await userEvent.type(screen.getByPlaceholderText("Enter name"), "James");
+    expect(hooks.validateOn.validate("name", "James")).toBe(
+      ValidateResult.Valid
+    );
   });
 });
