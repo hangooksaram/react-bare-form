01bfe005b00c2f32a62e1e02dd3ab286bcb677dd
오현재 <hyunjae1995@gmail.com>
2025-08-31 15:59:55 +0900

    refactor:utilRef -> utilRefFuncs

--- src/__tests__/form/useRef.test.tsx
+++ src/__tests__/form/useRef.test.tsx
@@ -4,17 +4,15 @@ import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/vitest";
 import TestForm from "@/test";
 import useRefs from "@/hooks/form/useRefs";
-import * as utilRef from "@/utils/ref";
+import * as utlRefFuncs from "@/utils/ref";
 
 describe("useRef", () => {
   let screen: ReturnType<typeof render>;
   let hook: ReturnType<typeof useRefs>;
-  let user: ReturnType<typeof userEvent.setup>;
 
   beforeEach(() => {
     vi.clearAllMocks();
     screen = render(<TestForm />);
-    user = userEvent.setup();
     hook = renderHook(() => useRefs()).result.current;
     window.HTMLElement.prototype.scrollIntoView = vi.fn();
   });
@@ -23,11 +21,11 @@ describe("useRef", () => {
     const firstInvalidInput = screen.getByLabelText("이메일");
     const submitButton = screen.getByRole("button", { name: "제출" });
 
-    vi.spyOn(utilRef, "scrollToElement");
+    vi.spyOn(utlRefFuncs, "scrollToElement");
 
     await userEvent.click(submitButton);
     await waitFor(() => {
-      expect(utilRef.scrollToElement).toBeCalledWith(firstInvalidInput);
+      expect(utlRefFuncs.scrollToElement).toBeCalledWith(firstInvalidInput);
     });
   });
 });
