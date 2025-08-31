e330161267e614bc19189832f8165eb1814861b9
오현재 <hyunjae1995@gmail.com>
2025-08-31 16:12:31 +0900

    test:should scroll to the first invalid element after validation failure, when elements are multiple

--- src/__tests__/form/useRef.test.tsx
+++ src/__tests__/form/useRef.test.tsx
@@ -7,18 +7,51 @@ import useRefs from "@/hooks/form/useRefs";
 import * as utlRefFuncs from "@/utils/ref";
 
 describe("useRef", () => {
-  let screen: ReturnType<typeof render>;
+  let screen: ReturnType<typeof render> = render(<TestForm />);
   let hook: ReturnType<typeof useRefs>;
 
   beforeEach(() => {
     vi.clearAllMocks();
-    screen = render(<TestForm />);
+
     hook = renderHook(() => useRefs()).result.current;
     window.HTMLElement.prototype.scrollIntoView = vi.fn();
   });
 
   it("should scroll to the first invalid element after validation failure", async () => {
     const firstInvalidInput = screen.getByLabelText("이메일");
+    const submitButton = screen.getByRole("button", { name: "제출" });
+
+    vi.spyOn(utlRefFuncs, "scrollToElement");
+
+    await userEvent.click(submitButton);
+    await waitFor(() => {
+      expect(utlRefFuncs.scrollToElement).toBeCalledWith(firstInvalidInput);
+    });
+  });
+
+  it("should scroll to the first invalid element after validation failure, when elements are multiple", async () => {
+    const validInputs = [
+      {
+        label: "이메일",
+        value: "test@example.com",
+      },
+      {
+        label: "이름",
+        value: "sdfsdfsdf",
+      },
+      {
+        label: "나이",
+        value: 20,
+      },
+    ];
+
+    for (const input of validInputs) {
+      const inputElement = screen.getByLabelText(input.label);
+      await userEvent.type(inputElement, String(input.value));
+    }
+
+    const firstInvalidInput = screen.getByLabelText("비밀번호");
+
     const submitButton = screen.getByRole("button", { name: "제출" });
 
     vi.spyOn(utlRefFuncs, "scrollToElement");
